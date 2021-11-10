bed7c8ee225bc322126a6800f2cb9bb8b5503e7b
Kshitija-patel <kshitija.patel.kp@gmail.com>
2021-11-09 23:35:39 -0500

    task 3:  PUSH some data

--- public/script.js
+++ public/script.js
@@ -18,18 +18,41 @@ const updateHTML = () => {
     for(let element of jsonData) {
         listHTML += `<li>`
         listHTML += `<button onClick="toggleImportance('${element.term}')">${element.term}</button>`
-        listHTML += `<span>${element.description} <a href="${element.url}">look here</a></span>`
+        listHTML += `<span id="${element.term}" contenteditable="true" onblur="updateContent('${element.term}')">${element.description}</span> <a href="${element.url}">look here</a>`
         listHTML += `<meter max="3" value="${element.importance}"></meter>`
         listHTML += `</li>`
     }
     listDataElement.innerHTML = listHTML;
 }
 
+const updateContent = (term) => {
+    for(let i=0; i<jsonData.length; i++) {
+        if(jsonData[i].term == term) {
+            jsonData[i].description = document.getElementById(term).innerHTML;
+            callModAPI(jsonData[i])
+            break;
+        }
+    }
+    console.log(jsonData)
+}
+
+const callModAPI = (body) => {
+    fetch("/mod", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }).then(response=>response.text()
+    ).then(html=>console.log(html))
+}
 const toggleImportance = (term) => {
     console.log(term)
     for(let i=0; i<jsonData.length; i++) {
         if(jsonData[i].term == term) {
             jsonData[i].importance = jsonData[i].importance == 3 ? 0 : jsonData[i].importance+1;
+            callModAPI(jsonData[i])
             break;
         }
     }
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express()
 
 // const path = require("path")
 const fs = require('fs')
+
+const bodyParser = require('body-parser')
 // const bodyParser = require('body-parser')
 let jdata = JSON.parse(fs.readFileSync('public/data.json'));
 
+let jsonParser = bodyParser.json()
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // use the express-static middleware
 app.use(express.static("public"))
@@ -15,6 +19,18 @@ app.use(express.static("public"))
 app.get('/info', (req,res)=> {
     res.json(jdata)
 })
+
+
+app.post('/mod', jsonParser, (req, res) => {
+    for(let i=0; i<jdata.data.length; i++) {
+        if(jdata.data[i].term == req.body.term) {
+            jdata.data[i].description = req.body.description
+            jdata.data[i].importance = req.body.importance
+        }
+    }
+    res.sendStatus(200)
+})
+
 
 // start the server listening for requests
 let listener = app.listen(3000, 
